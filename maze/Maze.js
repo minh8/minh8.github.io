e71534153521c7e4759d5da7cb3d472883541c08
Đào Nhật Minh <57982441+minh8@users.noreply.github.com>
2020-02-23 10:03:33 +0700

    Update Maze.js

--- maze/Maze.js
+++ maze/Maze.js
@@ -13,7 +13,7 @@ class Maze {
     this.margin = 10;
     this.top = 90;
     this.data = [];
-    this.columns = int(random(160) + 4);
+    this.columns = int(random(260) + 4);
     this.maxRecursion = 100;
     this.currentRecursion = 0;
 
